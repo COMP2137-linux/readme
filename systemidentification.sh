8f0a60ee5d3c1d58a997c15f10fb5cc845aba1d6
amandeep <kauramandeep0565@gmail.com>
2023-05-30 17:30:12 -0400

    created and tested sytem identification script on may 30th

---
+++ systemidentification.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#to chech ip address
+ip add
+
+#to check default ip gateway
+ip route
+
+#to check hostname
+hostname
+
+#to check whether computer is online or offline
+systemctl status
+
+
